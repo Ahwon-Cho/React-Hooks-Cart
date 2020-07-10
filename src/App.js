7d5ccf00737a9ffa8329ccb9c3467bb62aabcaf5
Ahwon-Cho <67344151+Ahwon-Cho@users.noreply.github.com>
2020-07-09 17:17:06 -0700

    added comment.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const items = [
   { id: 1, name: "Taco Seasoning", price: 2.25, qty: 3 },
   { id: 1, name: "Apple", price: 1.25, qty: 10 }
 ];
-
+// main
 export default function App() {
   return (
     <div className="App">
