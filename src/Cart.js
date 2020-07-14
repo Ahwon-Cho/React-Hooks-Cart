2724be30cd3fe7842b9249c82c87223a64be85bc
Ahwon-Cho <67344151+Ahwon-Cho@users.noreply.github.com>
2020-07-14 16:48:50 -0700

    initial commit

--- src/Cart.js
+++ src/Cart.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import CartItem from "./CartItem";
 
 function Cart({ initialItems }) {
-  const grandTotal = initialItems.reduce(
+  const [items, setItems] = useState(initialItems);
+
+  const updateQty = (id, newQty) => {
+    const newItems = items.map(items => {
+      if (item.id === id) {
+        return { ...item, qty: newQty };
+      }
+      return item;
+    });
+    setItems(newItems);
+  };
+
+  const grandTotal = items.reduce(
     (total, item) => total + item.qty * item.price,
     0
   );
@@ -10,9 +22,9 @@ function Cart({ initialItems }) {
     <div>
       <h1>Hello Cart!</h1>
       <ul>
-        {initialItems.map(item => (
+        {items.map(item => (
           <div className="Cart-item">
-            <CartItem key={item.id} {...item} />
+            <CartItem key={item.id} updateQty={updateQty} {...item} />
           </div>
         ))}
       </ul>
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,14 +1,19 @@
 import React from "react";
 
-function CartItem({ id, name, price, qty }) {
+function CartItem({ id, name, price, qty, updateQty }) {
+  const addOne = () => updateQty(id, qty + 1);
+  const substractionOne = () => updateQty(id, qty - 1);
+
   return (
     <div>
       <div>{name}</div>
       <div>${price}</div>
       <div>
-        <button>-</button>
+        <button onClick={substractionOne} disabled={qty < 0}>
+          -
+        </button>
         {qty}
-        <button>+</button>
+        <button onClick={addOne}>+</button>
       </div>
       <div>Total: {qty * price}</div>
     </div>
